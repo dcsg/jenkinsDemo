3a3f2067241c83ca34e3dfa433391c10ba8070a1
Daniel Gomes <daniel.gomes@reset.pt>
2012-01-29 15:30:40 +0000

    added more tests

--- tests/MyClassTest.php
+++ tests/MyClassTest.php
@@ -7,5 +7,7 @@ class MyClassTest extends PHPUnit_Framework_TestCase {
     public function testDemo() {
         $x = new MyClass();
         $this->assertEquals(1, $x->demo(1));
+        $this->assertTrue(3, $x->demo(1));
+        $this->assertFalse(3, $x->demo(1));
     }
 }
